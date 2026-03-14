82ab0f27850557c8a6ccadb60d8a1140964d1527
hiverkiya <hiverkiya@gmail.com>
2026-03-14 19:44:12 +0530

    tRPC - Data access layer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 import { Button } from "@/components/ui/button";
-
-const Page = () => {
+import { caller } from "@/trpc/server";
+const Page = async () => {
+  const users = await caller.getUsers();
   return (
-    <div className="text-red-500">
-      Hello world
-      <Button variant="outline">Hello</Button>
+    <div className="text-red-400 min-h-screen bg-black">
+      Calling the database
+      <Button variant="outline">{JSON.stringify(users)}</Button>
     </div>
   );
 };
