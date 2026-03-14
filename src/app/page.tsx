17a6db3b6b297f1c5f4ac1a42a3d128013ddde7b
hiverkiya <hiverkiya@gmail.com>
2026-03-14 16:54:20 +0530

    Setting up basic shadcn and nextjs application

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+const Page = () => {
+  return (
+    <div className="text-red-500">
+      Hello world
+      <Button variant="outline">Hello</Button>
+    </div>
+  );
+};
+export default Page;
