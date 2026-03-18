dfa73b52b947d2b29e66aa077169544cf1e0d7e4
hiverkiya <hiverkiya@gmail.com>
2026-03-18 14:24:35 +0530

    Designed the sidebar layout

---
+++ src/app/(dashboard)/(rest)/credentials/[credentialId]/page.tsx
@@ -0,0 +1,13 @@
+import { requireAuth } from "@/lib/auth-utils";
+
+interface PageProps {
+  params: Promise<{
+    credentialId: string;
+  }>;
+}
+const Page = async ({ params }: PageProps) => {
+  await requireAuth();
+  const { credentialId } = await params;
+  return <div>Credential ID: {credentialId}</div>;
+};
+export default Page;
---
+++ src/app/(dashboard)/(rest)/executions/page.tsx
@@ -0,0 +1,7 @@
+import { requireAuth } from "@/lib/auth-utils";
+
+const Page = async () => {
+  await requireAuth();
+  return <div>Executions</div>;
+};
+export default Page;
